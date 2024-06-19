47acdd7210e29c088858bc573d8fbaaad09f1f7d
Dylan Jay <software@pretaweb.com>
2024-06-19 13:45:04 +0700

    correct test loginname

--- packages/volto-hydra/src/customizations/components/theme/Login/Login.jsx
+++ packages/volto-hydra/src/customizations/components/theme/Login/Login.jsx
@@ -151,7 +151,7 @@ const Login = (props) => {
                 <a href="https://github.com/collective/volto-hydra">progress</a>
               </p>
               <p>
-                Login below with "hydraeditor" "hydraeditor" to see the progress
+                Login below with username "hydraedit" and password "hydraedit" to see the progress
                 of the editor experience. You can try any of the following
                 frontends:
               </p>
@@ -174,7 +174,7 @@ const Login = (props) => {
                 doesn't are in the{' '}
                 <a href="https://github.com/collective/volto-hydra">
                   Hydra README
-                </a>
+                </a>. Report bugs to <a href="https://github.com/collective/volto-hydra/issues">Hydra Issues</a>.
               </p>
             </Segment>
           </Grid.Column>
