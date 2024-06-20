29ee46c3c25faba004d6c44a0a0cb9a494879138
MAX-786 <hussainmohammad328@gmail.com>
2024-06-20 19:47:10 +0530

    window ref error fix

--- packages/volto-hydra/src/components/Iframe/View.jsx
+++ packages/volto-hydra/src/components/Iframe/View.jsx
@@ -20,7 +20,9 @@ const getDefualtUrl = () =>
  * @returns {string} URL with the admin params
  */
 const getUrlWithAdminParams = (url, token) => {
-  return `${url}${window.location.pathname.replace('/edit', '')}?access_token=${token}&_edit=true`;
+  return typeof window !== 'undefined'
+    ? `${url}${window.location.pathname.replace('/edit', '')}?access_token=${token}&_edit=true`
+    : null;
 };
 
 function isValidUrl(string) {
@@ -47,52 +49,6 @@ const Iframe = () => {
     ? getUrlWithAdminParams(savedUrl, token)
     : getUrlWithAdminParams(defaultUrl, token);
 
-  setUrl(
-    `${savedUrl || defaultUrl}${window.location.pathname.replace('/edit', '')}`,
-  );
-  setSrc(initialUrl);
-
-  useEffect(() => {
-    const initialUrlOrigin = new URL(initialUrl).origin;
-    const messageHandler = (event) => {
-      if (event.origin !== initialUrlOrigin) {
-        return;
-      }
-      const { type } = event.data;
-      switch (type) {
-        case 'URL_CHANGE': // URL change from the iframe
-          setUrl(event.data.url);
-          handleNavigateToUrl(event.data.url);
-          break;
-
-        default:
-          break;
-      }
-    };
-
-    // Listen for messages from the iframe
-    window.addEventListener('message', messageHandler);
-
-    // Clean up the event listener on unmount
-    return () => {
-      window.removeEventListener('message', messageHandler);
-    };
-  }, [token]);
-
-  useEffect(() => {
-    if (Object.keys(form).length > 0 && isValidUrl(initialUrl)) {
-      // Send the form data to the iframe
-      const origin = new URL(initialUrl).origin;
-      document
-        .getElementById('previewIframe')
-        .contentWindow.postMessage({ type: 'FORM', data: form }, origin);
-    }
-  }, [form, initialUrl]);
-
-  const handleUrlChange = (event) => {
-    setUrl(event.target.value);
-  };
-
   const handleNavigateToUrl = useCallback(
     (givenUrl = null) => {
       if (!isValidUrl(givenUrl) && !isValidUrl(url)) {
@@ -100,7 +56,6 @@ const Iframe = () => {
       }
       // Update adminUI URL with the new URL
       const formattedUrl = givenUrl ? new URL(givenUrl) : new URL(url);
-      // setSrc(getUrlWithAdminParams(formattedUrl.origin, token));
       const newOrigin = formattedUrl.origin;
       Cookies.set('iframe_url', newOrigin, { expires: 7 });
 
@@ -118,6 +73,53 @@ const Iframe = () => {
     },
     [history, url],
   );
+
+  useEffect(() => {
+    setUrl(
+      `${savedUrl || defaultUrl}${window.location.pathname.replace('/edit', '')}`,
+    );
+    setSrc(initialUrl);
+  }, [savedUrl, defaultUrl, initialUrl]);
+
+  useEffect(() => {
+    const initialUrlOrigin = new URL(initialUrl).origin;
+    const messageHandler = (event) => {
+      if (event.origin !== initialUrlOrigin) {
+        return;
+      }
+      const { type } = event.data;
+      switch (type) {
+        case 'URL_CHANGE': // URL change from the iframe
+          handleNavigateToUrl(event.data.url);
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    // Listen for messages from the iframe
+    window.addEventListener('message', messageHandler);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('message', messageHandler);
+    };
+  }, [handleNavigateToUrl, initialUrl, token]);
+
+  useEffect(() => {
+    if (Object.keys(form).length > 0 && isValidUrl(initialUrl)) {
+      // Send the form data to the iframe
+      const origin = new URL(initialUrl).origin;
+      document
+        .getElementById('previewIframe')
+        .contentWindow.postMessage({ type: 'FORM', data: form }, origin);
+    }
+  }, [form, initialUrl]);
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value);
+  };
 
   return (
     <div id="iframeContainer">
