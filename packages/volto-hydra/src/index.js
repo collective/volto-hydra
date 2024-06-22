ba2e02170080fd1d0ddeb6f5139f553eb90f8a81
MAX-786 <hussainmohammad328@gmail.com>
2024-06-22 22:02:27 +0530

    edit text from sidebar and see data updated in realtime

--- packages/volto-hydra/src/index.js
+++ packages/volto-hydra/src/index.js
@@ -1,7 +1,24 @@
 import selectedBlock from './reducers';
 
 const applyConfig = (config) => {
+  // Add the selectedBlock reducer
   config.addonReducers.selectedBlock = selectedBlock;
+  // Add the slate block in the sidebar
+  config.blocks.blocksConfig.slate.schemaEnhancer = ({
+    formData,
+    schema,
+    intl,
+  }) => {
+    schema.properties.value = {
+      title: 'Body',
+      widget: 'slate',
+    };
+    schema.fieldsets[0].fields.push('value');
+    return schema;
+  };
+  // Set the sidebarTab to 1
+  config.blocks.blocksConfig.slate.sidebarTab = 1;
+
   return config;
 };
 
