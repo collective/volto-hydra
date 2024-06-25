e00965c524b24dbb9601a24d1a55938784ad7b47
MAX-786 <hussainmohammad328@gmail.com>
2024-06-25 14:26:44 +0530

    update schema

--- packages/volto-hydra/src/index.js
+++ packages/volto-hydra/src/index.js
@@ -1,7 +1,40 @@
 import selectedBlock from './reducers';
 
 const applyConfig = (config) => {
+  // Add the selectedBlock reducer
   config.addonReducers.selectedBlock = selectedBlock;
+
+  // Add the slate block in the sidebar
+  config.blocks.blocksConfig.slate.schemaEnhancer = ({
+    formData,
+    schema,
+    intl,
+  }) => {
+    schema.properties.value = {
+      title: 'Body',
+      widget: 'slate',
+    };
+    schema.fieldsets[0].fields.push('value');
+    return schema;
+  };
+
+  // Set the sidebarTab to 1
+  config.blocks.blocksConfig.slate.sidebarTab = 1;
+
+  // Add image from sidebar
+  config.blocks.blocksConfig.image.schemaEnhancer = ({
+    formData,
+    schema,
+    intl,
+  }) => {
+    schema.properties.url = {
+      title: 'Image Src',
+      widget: 'image',
+    };
+    schema.fieldsets[0].fields.push('url');
+    return schema;
+  };
+
   return config;
 };
 
