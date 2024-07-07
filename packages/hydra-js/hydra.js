fe5e51203cca8e1a03122403f07078918df0b679
MAX-786 <hussainmohammad328@gmail.com>
2024-07-07 18:49:13 +0530

    Add nodeIds to Selected Block's children in formData

--- packages/hydra-js/hydra.js
+++ packages/hydra-js/hydra.js
@@ -261,6 +261,38 @@ class Bridge {
     observer.observe(document.body, { childList: true, subtree: true });
   }
 
+  /**
+   * Add nodeIds in the json object to each of the Selected Block's children
+   * @param {JSON} json Selected Block's data
+   * @param {BigInteger} nodeIdCounter (Optional) Counter to keep track of the nodeIds
+   * @returns {JSON} block's data with nodeIds added
+   */
+  addNodeIds(json, nodeIdCounter = { current: 0 }) {
+    if (Array.isArray(json)) {
+      return json.map((item) => this.addNodeIds(item, nodeIdCounter));
+    } else if (typeof json === 'object' && json !== null) {
+      // Clone the object to ensure it's extensible
+      json = JSON.parse(JSON.stringify(json));
+
+      if (json.hasOwnProperty('data')) {
+        json.nodeId = nodeIdCounter.current++;
+        for (const key in json) {
+          if (json.hasOwnProperty(key) && key !== 'nodeId' && key !== 'data') {
+            json[key] = this.addNodeIds(json[key], nodeIdCounter);
+          }
+        }
+      } else {
+        json.nodeId = nodeIdCounter.current++;
+        for (const key in json) {
+          if (json.hasOwnProperty(key) && key !== 'nodeId') {
+            json[key] = this.addNodeIds(json[key], nodeIdCounter);
+          }
+        }
+      }
+    }
+    return json;
+  }
+
   injectCSS() {
     const style = document.createElement('style');
     style.type = 'text/css';
