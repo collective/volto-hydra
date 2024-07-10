9f062171dedcb28193b59f3a6298839f57fe5bb8
Jefferson Bledsoe <me@jeffersonbledsoe.com>
2024-07-10 12:48:25 +0100

    Upgrade Volto to 18.0.0.alpha41 (#101)
    * Fix example imports
    
    * Bump volto version to 18 alpha41
    
    * Update import
    
    * Remove unnecessary react-intl pin to fix version change

--- examples/hydra-nextjs/src/app/[blogs]/page.js
+++ examples/hydra-nextjs/src/app/[blogs]/page.js
@@ -1,7 +1,7 @@
 'use client';
 import { notFound } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { initBridge, getTokenFromCookie } from '#utils/hydra';
+import { initBridge, getTokenFromCookie } from '@volto-hydra/hydra-js';
 import BlocksList from '@/components/BlocksList';
 import { fetchContent } from '#utils/api';
 
