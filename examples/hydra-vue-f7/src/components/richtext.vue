b25e759c4a064bd7a63ea5177c192361d764338e
MAX-786 <hussainmohammad328@gmail.com>
2024-08-23 07:33:57 +0530

    update richtextlvue in f7 to render slate properly

--- examples/hydra-vue-f7/src/components/richtext.vue
+++ examples/hydra-vue-f7/src/components/richtext.vue
@@ -1,22 +1,33 @@
 <template>
-    <f7-link v-if="node.type =='link'" :href="node.data.url" external :data-node-id="node['nodeId']">{{ node.text }}<RichText v-for="child in subs" :key="child" :node="child"/></f7-link>
-    <template v-else-if="!node.type">{{ node.text }}</template>
-    <component v-else :is="node.type" :data-node-id="node['nodeId']">{{ node.text }}<RichText v-for="child in subs" :key="child" :node="child"/></component>
+  <template v-if="node.type === 'link'">
+    <f7-link :href="node.data.url" external :data-node-id="node.nodeId">
+      {{ node.text }}
+      <RichText v-for="child in subs" :key="child.nodeId" :node="child" />
+    </f7-link>
+  </template>
+  <span v-else-if="!node.type" :data-node-id="node.nodeId">
+    {{ node.text }}
+  </span>
+  <component v-else :is="node.type" :data-node-id="node.nodeId">
+    {{ node.text }}
+    <RichText v-for="child in subs" :key="child.nodeId" :node="child" />
+  </component>
 </template>
+
 <script>
 export default {
   name: 'RichText',
   props: {
     node: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   computed: {
     subs() {
-      const { children } = this.node
-      return children && children || []
-    }
-  }
-}
+      const { children } = this.node;
+      return (children && children) || [];
+    },
+  },
+};
 </script>
