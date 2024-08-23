6935fb0834938bf4c3c2ebe5416f2b895f6db9b2
MAX-786 <hussainmohammad328@gmail.com>
2024-08-24 01:53:24 +0530

    ui fixes and adding cancel btn

--- packages/hydra-js/hydra.js
+++ packages/hydra-js/hydra.js
@@ -481,17 +481,20 @@ class Bridge {
       linkButton.addEventListener('click', () => {
         const selection = window.getSelection();
         if (!selection.rangeCount || selection.isCollapsed) return;
+        const listenClickOutside = (e) => {
+          if (!e.target.closest('.link-input-container')) {
+            linkButton.classList.remove('active');
+            container?.remove();
+            if (this.quantaToolbar)
+              this.quantaToolbar.style.visibility = 'visible';
+          }
+        };
+        document.addEventListener('click', listenClickOutside, { once: true });
         const range = selection.getRangeAt(0);
-        if (currentFormats['link'].present) {
-          this.unwrapFormatting(range, 'link');
-          this.sendFormattedHTMLToAdminUI(selection);
-          linkButton.classList.remove('active');
-          return;
-        }
 
         linkButton.classList.add('active');
-
-        const commonAncestor = selection.getRangeAt(0).commonAncestorContainer;
+        this.quantaToolbar.style.visibility = 'hidden';
+        const commonAncestor = range.commonAncestorContainer;
 
         const container = document.createElement('div');
         container.classList.add('link-input-container');
@@ -499,20 +502,39 @@ class Bridge {
         const inputField = document.createElement('input');
         inputField.classList.add('link-input');
         inputField.type = 'text';
-        inputField.placeholder = 'Enter URL';
+        inputField.placeholder = 'Type a URL...';
         inputField.addEventListener('keydown', (e) => {
           if (e.key === 'Escape') {
             linkButton.classList.remove('active');
             container.remove();
+            this.quantaToolbar.style.visibility = 'visible';
+          } else if (e.key === 'Enter') {
+            e.preventDefault();
+            submitBtn.click();
+          } else if (e.key === 'Tab') {
+            e.preventDefault();
+            inputField.blur();
+            if (inputField.nextSibling.classList.contains('hide'))
+              cancelBtn.focus();
+            else submitBtn.focus();
           }
         });
-
+        inputField.addEventListener('input', (e) => {
+          const currentValue = e.target.value;
+          if (currentValue === '') {
+            submitBtn.classList.add('hide');
+            cancelBtn.classList.remove('hide');
+          } else {
+            submitBtn.classList.remove('hide');
+            cancelBtn.classList.add('hide');
+          }
+        });
         const folderBtn = document.createElement('button');
         folderBtn.classList.add('link-folder-btn');
         folderBtn.innerHTML = linkFolderSVG;
 
         const submitBtn = document.createElement('button');
-        submitBtn.classList.add('link-submit-btn');
+        submitBtn.classList.add('link-submit-btn', 'hide');
         submitBtn.innerHTML = linkSubmitSVG;
         submitBtn.addEventListener('click', () => {
           const url = inputField.value;
@@ -521,7 +543,7 @@ class Bridge {
           range.surroundContents(link);
           this.isInlineEditing = false;
           const editableParent = this.findEditableParent(commonAncestor);
-          const htmlString = editableParent.outerHTML;
+          const htmlString = editableParent?.outerHTML;
 
           window.parent.postMessage(
             {
@@ -532,11 +554,43 @@ class Bridge {
           );
           linkButton.classList.remove('active');
           container.remove(); // Close the input field (why errrrror? whyyy..)(sometimes)
+          this.quantaToolbar.style.visibility = 'visible';
+        });
+
+        const cancelBtn = document.createElement('button');
+        cancelBtn.classList.add('link-cancel-btn');
+        cancelBtn.innerHTML = linkCancelSVG;
+        cancelBtn.addEventListener('click', () => {
+          if (currentFormats['link'].present) {
+            this.unwrapFormatting(range, 'link');
+            this.sendFormattedHTMLToAdminUI(selection);
+          }
+          linkButton.classList.remove('active');
+          container.remove();
+          this.quantaToolbar.style.visibility = 'visible';
+          return;
         });
 
         container.appendChild(folderBtn);
         container.appendChild(inputField);
         container.appendChild(submitBtn);
+        container.appendChild(cancelBtn);
+
+        if (currentFormats['link'].present) {
+          if (currentFormats['link'].enclosing) {
+            inputField.value = commonAncestor.parentNode
+              ?.closest('a')
+              ?.getAttribute('href');
+          } else {
+            const children = commonAncestor.children;
+            for (let i = 0; i < children.length; i++) {
+              if (children[i].tagName === 'A') {
+                inputField.value = children[i].getAttribute('href');
+                break;
+              }
+            }
+          }
+        }
 
         // const buttonRect = linkButton.getBoundingClientRect();
         // container.style.position = "absolute";
@@ -578,6 +632,7 @@ class Bridge {
       this.handleMouseUp = (e) => {
         if (
           e.target.closest('[data-editable-field="value"]') &&
+          !e.target.closest('.volto-hydra-quantaToolbar') &&
           e.target
             .closest('[data-block-uid]')
             .getAttribute('data-block-uid') === blockUid
@@ -625,8 +680,19 @@ class Bridge {
     dropdownMenu.appendChild(removeOption);
 
     // Add event listener to toggle dropdown visibility
-    menuButton.addEventListener('click', () => {
+    menuButton.addEventListener('click', (e) => {
+      e.stopPropagation();
       dropdownMenu.classList.toggle('visible');
+      document.addEventListener(
+        'click',
+        (e) => {
+          if (!e.target.closest('.volto-hydra-dropdown-menu')) {
+            dropdownMenu.classList.remove('visible');
+          }
+          console.log('outside click');
+        },
+        { once: true },
+      );
     });
 
     // Append elements to the quantaToolbar
@@ -768,7 +834,11 @@ class Bridge {
           window.location.origin,
         );
         // console.log("select block", event.data?.method);
-
+        const blockElement = document.querySelector(
+          `[data-block-uid="${uid}"]`,
+        );
+        !this.elementIsVisibleInViewport(blockElement) &&
+          blockElement.scrollIntoView();
         // this.isInlineEditing = true;
         // this.observeForBlock(uid);
       }
@@ -1494,8 +1564,8 @@ class Bridge {
         }
         .link-input-container {
           position: absolute;
-          top: -45px;
-          left: 60px;
+          top: 0;
+          left: 0;
           width: fit-content;
           display: flex;
           background: white;
@@ -1505,6 +1575,7 @@ class Bridge {
           align-items: center;
           justify-content: center;
           box-sizing: border-box;
+          visibility: visible;
         }
         .link-input {
           height: 40px;
@@ -1518,22 +1589,33 @@ class Bridge {
         .link-input:focus {
           outline: none;
         }
+        .link-input::placeholder {
+          color: #0B78D0;
+        }
         .link-folder-btn,
-        .link-submit-btn {
-          width: 36px;
-          height: 36px;
+        .link-submit-btn,
+        .link-cancel-btn {
+          width: 32px;
+          height: 32px;
           border: none;
           background: white;
           cursor: pointer;
           border-radius: 8px;
+          margin-right: 2px
         }
         .link-folder-btn:hover,
-        .link-submit-btn:hover {
+        .link-submit-btn:hover,
+        .link-cancel-btn:hover {
           background-color: #ddd;
         }
-        .link-submit-btn {
+        .link-submit-btn,
+        .link-cancel-btn {
           border-radius: 100%;
           background-color: #0B78D0;
+        }
+        .link-cancel-btn.hide,
+        .link-submit-btn.hide {
+          display: none;
         }
         .volto-hydra-dropdown-menu {
           display: none;
@@ -1673,5 +1755,8 @@ const settingsSVG = `<svg width="18px" height="18px" viewBox="0 0 24 24" fill="n
   <circle cx="12" cy="12" r="3" stroke="#1C274C" stroke-width="1.5"/>
   <path d="M13.7654 2.15224C13.3978 2 12.9319 2 12 2C11.0681 2 10.6022 2 10.2346 2.15224C9.74457 2.35523 9.35522 2.74458 9.15223 3.23463C9.05957 3.45834 9.0233 3.7185 9.00911 4.09799C8.98826 4.65568 8.70226 5.17189 8.21894 5.45093C7.73564 5.72996 7.14559 5.71954 6.65219 5.45876C6.31645 5.2813 6.07301 5.18262 5.83294 5.15102C5.30704 5.08178 4.77518 5.22429 4.35436 5.5472C4.03874 5.78938 3.80577 6.1929 3.33983 6.99993C2.87389 7.80697 2.64092 8.21048 2.58899 8.60491C2.51976 9.1308 2.66227 9.66266 2.98518 10.0835C3.13256 10.2756 3.3397 10.437 3.66119 10.639C4.1338 10.936 4.43789 11.4419 4.43786 12C4.43783 12.5581 4.13375 13.0639 3.66118 13.3608C3.33965 13.5629 3.13248 13.7244 2.98508 13.9165C2.66217 14.3373 2.51966 14.8691 2.5889 15.395C2.64082 15.7894 2.87379 16.193 3.33973 17C3.80568 17.807 4.03865 18.2106 4.35426 18.4527C4.77508 18.7756 5.30694 18.9181 5.83284 18.8489C6.07289 18.8173 6.31632 18.7186 6.65204 18.5412C7.14547 18.2804 7.73556 18.27 8.2189 18.549C8.70224 18.8281 8.98826 19.3443 9.00911 19.9021C9.02331 20.2815 9.05957 20.5417 9.15223 20.7654C9.35522 21.2554 9.74457 21.6448 10.2346 21.8478C10.6022 22 11.0681 22 12 22C12.9319 22 13.3978 22 13.7654 21.8478C14.2554 21.6448 14.6448 21.2554 14.8477 20.7654C14.9404 20.5417 14.9767 20.2815 14.9909 19.902C15.0117 19.3443 15.2977 18.8281 15.781 18.549C16.2643 18.2699 16.8544 18.2804 17.3479 18.5412C17.6836 18.7186 17.927 18.8172 18.167 18.8488C18.6929 18.9181 19.2248 18.7756 19.6456 18.4527C19.9612 18.2105 20.1942 17.807 20.6601 16.9999C21.1261 16.1929 21.3591 15.7894 21.411 15.395C21.4802 14.8691 21.3377 14.3372 21.0148 13.9164C20.8674 13.7243 20.6602 13.5628 20.3387 13.3608C19.8662 13.0639 19.5621 12.558 19.5621 11.9999C19.5621 11.4418 19.8662 10.9361 20.3387 10.6392C20.6603 10.4371 20.8675 10.2757 21.0149 10.0835C21.3378 9.66273 21.4803 9.13087 21.4111 8.60497C21.3592 8.21055 21.1262 7.80703 20.6602 7C20.1943 6.19297 19.9613 5.78945 19.6457 5.54727C19.2249 5.22436 18.693 5.08185 18.1671 5.15109C17.9271 5.18269 17.6837 5.28136 17.3479 5.4588C16.8545 5.71959 16.2644 5.73002 15.7811 5.45096C15.2977 5.17191 15.0117 4.65566 14.9909 4.09794C14.9767 3.71848 14.9404 3.45833 14.8477 3.23463C14.6448 2.74458 14.2554 2.35523 13.7654 2.15224Z" stroke="#1C274C" stroke-width="1.5"/>
   </svg>`;
-const linkSubmitSVG = `<img width="20px" height="20px" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAtElEQVR4nO3YPQrCQBRF4VPpfiytLSyMC1BSGLNVK7GwcQFWgrV/hUYCU4hoIYGY+7gfvAUcZpiZBMzMzMzMOmgIbNKMELYDqjQ3IEPU/iVEOmYG3D/ETBC0+BIzRZBj1FYmQ5BjfpEDx7elb3MuwJiGesD5jxFVmmvTe6YPnCKE1ObAQX1rtamI8HQpItwlS0d0RBnhBVxGiMiBh/rpFOpTdxshojYAVsBa/XeQmZmZmaHlCeC06ncEGe4qAAAAAElFTkSuQmCC">`;
+const linkSubmitSVG = `<img width="20px" height="20px" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNiAxMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTkuMDc2OTIgMTAuNDEwOEwxMC4yNTU0IDExLjU4OTNMMTUuODQ0NyA2LjAwMDAyTDEwLjI1NTQgMC40MTA3NjdMOS4wNzY5MiAxLjU4OTI4TDEyLjY1NDMgNS4xNjY2OUgwLjQ5OTUxMlY2LjgzMzM1SDEyLjY1NDNMOS4wNzY5MiAxMC40MTA4WiIgZmlsbD0id2hpdGUiLz4KPC9zdmc+Cg==" />`;
 const linkFolderSVG = `<img width="20px" height="20px" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAaklEQVR4nO2WywmAQAwF52IXWoBFWIvl2JFWJnh74nkFXQmExTeQc4b8CBhTopuYSRbYgSlTQMABLMAIdBkCCgoLPKK3paqkugXRWEBuAR5CvIb4EKmpU9wHJh++CKxBElfyramHxJh/cQJpdrBykxDdigAAAABJRU5ErkJggg==">`;
+const linkCancelSVG = `<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+<g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M7,4c-0.25587,0 -0.51203,0.09747 -0.70703,0.29297l-2,2c-0.391,0.391 -0.391,1.02406 0,1.41406l7.29297,7.29297l-7.29297,7.29297c-0.391,0.391 -0.391,1.02406 0,1.41406l2,2c0.391,0.391 1.02406,0.391 1.41406,0l7.29297,-7.29297l7.29297,7.29297c0.39,0.391 1.02406,0.391 1.41406,0l2,-2c0.391,-0.391 0.391,-1.02406 0,-1.41406l-7.29297,-7.29297l7.29297,-7.29297c0.391,-0.39 0.391,-1.02406 0,-1.41406l-2,-2c-0.391,-0.391 -1.02406,-0.391 -1.41406,0l-7.29297,7.29297l-7.29297,-7.29297c-0.1955,-0.1955 -0.45116,-0.29297 -0.70703,-0.29297z"></path></g></g>
+</svg>`;
