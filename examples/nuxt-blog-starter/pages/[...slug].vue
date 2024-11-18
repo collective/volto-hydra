566a20253fbbb50c34942181026adf6417059fa5
Dylan Jay <software@pretaweb.com>
2024-11-18 12:39:51 +0700

    add readme links

--- examples/nuxt-blog-starter/pages/[...slug].vue
+++ examples/nuxt-blog-starter/pages/[...slug].vue
@@ -26,16 +26,10 @@
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-                <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                <a href="https://github.com/collective/volto-hydra" class="hover:underline me-4 md:me-6">About</a>
             </li>
             <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
+                <a href="https://github.com/collective/volto-hydra" class="hover:underline">Contact</a>
             </li>
         </ul>
         </div>
